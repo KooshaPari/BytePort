690ff9581311155f5cc6e339e964c805acd4c557
Koosha Paridehpour <kooshapari@gmail.com>
2024-12-29 19:06:43 -0800

    Partial User CRUD / Addtl LLM Providers

--- .history/backend/byteport/lib/git_20241126140035.go
+++
@@ -1,8 +0,0 @@
-package lib
-
-// LinkWithGithub redirects the user to GitHub for app installation.
-func LinkWithGithub(c *gin.Context, user models.User) {
-	appName := "byteport-gh"
-	redirectURL := fmt.Sprintf("https://github.com/apps/%s/installations/new?state=%s", appName, user.UUID)
-	c.Redirect(http.StatusFound, redirectURL)
-}
